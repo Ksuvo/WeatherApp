164a75732b372af86e3eef7a397319b19c55d1ed
Ksuvo <qrawesvlad@gmail.com>
2025-04-13 19:35:07 +0300

    third

--- link.js
+++ link.js
@@ -9,6 +9,7 @@ async function getData(url) {
     }
 
     const json = await response.json();
+    console.log(json)
     return json;
   } catch (error) {
     console.error(error.message);
@@ -19,10 +20,17 @@ window.onload = (e) =>{
     document.getElementById("gorod").addEventListener('keyup', (e) =>{
         if (e.key == 'Enter'){
                 let city = document.getElementById('gorod').value
-                const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`
+                const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7&aqi=no&alerts=yes`
                 getData(apiUrl).then(function(result){
                     document.getElementById("town").innerText = result.location.name
-                    document.getElementById("tempa").innerText = result.current.temp_c  
+                    document.getElementById("tempa").innerText = Math.round(result.current.temp_c)+" °C"
+                    document.getElementById("h2").innerText = result.location.localtime
+                    document.getElementById("icon").style.backgroundImage=`url(${result.current.condition.icon})`
+                    for (let i = 0; i < 7; i++) {
+                      document.getElementById(`${i}`).firstChild.innerText = result.forecast.forecastday[i].date.substr(8, 9 )
+                      document.getElementById(`${i}`).firstChild.innerText += " "+Math.round(result.forecast.forecastday[i].day.avgtemp_c)+" °C"
+                      document.getElementById(`icon${i}`).style.backgroundImage=`url(${result.forecast.forecastday[i].day.condition.icon})`
+                    }
                 })
         }
     })
